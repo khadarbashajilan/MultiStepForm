ac0aaffb5f59905e546876dd179bdc0fe29cd012
M KHADAR BASHA JILAN <khadarbashajilan001@gmail.com>
2025-07-21 18:04:41 +0530

    usereducer hook implemented + understood

--- src/pages/Address.tsx
+++ src/pages/Address.tsx
@@ -1,10 +1,81 @@
-import React from 'react'
+import React, { useReducer } from 'react'
+import { useNavigate } from 'react-router-dom';
+
+interface FormData{
+  address: {
+    street: string;
+    land: string;
+    zip: number;
+  }
+}
+
+const initial : FormData["address"] ={
+  street: "",
+  land: "",
+  zip: 0,
+}
+
+interface FormAction {
+  type: "UPDATE_ADDRESS",
+  payload: Partial<FormData["address"]>
+}
 
 const Address = () => {
+
+  const [state, addressDispatch] = useReducer(addressReducer, initial);
+
+  function addressReducer(state = initial, action: FormAction) {
+    return { ...state, ...action.payload };
+  }
+
+  const navigate = useNavigate();
+
+  function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
+    e.preventDefault();
+    console.log(state); // -> {street: ---, land: ---, zip: 12345}
+    navigate("/preferences");
+  }
   return (
-    <div>
-      Adaress
-    </div>
+    <form onSubmit={(e) => handleSubmit(e)}>
+      <h1>Adress</h1>
+      <div>
+        <label htmlFor="street">Street</label>
+        <input type="text" id='street'
+        onChange={(e)=>
+          addressDispatch({
+            type: "UPDATE_ADDRESS",
+            payload:{street: e.target.value},
+          })
+        }
+        />
+      </div>
+      <div>
+        <label htmlFor="land">LandMark</label>
+        <input type="text" id='land'
+        onChange={(e)=>
+          addressDispatch({
+            type:"UPDATE_ADDRESS",
+            payload:{land: e.target.value},
+          })
+        }
+        />
+      </div>
+      <div>
+        <label htmlFor="zip">Zip Code</label>
+        <input type="number" id='ip'
+        onChange={(e)=>
+          addressDispatch({
+            type:"UPDATE_ADDRESS",
+            payload:{zip: Number(e.target.value)},
+          })
+        }
+        />
+      </div>
+      <div>
+        <button onClick={()=>{navigate("/"); console.log(state)}} type='button'>Back</button>
+        <button type='submit'>Next</button>
+      </div>
+    </form>
   )
 }
 
--- src/pages/PersonalInfo.tsx
+++ src/pages/PersonalInfo.tsx
@@ -1,22 +1,82 @@
+import { useReducer } from "react"
+import {  useNavigate } from "react-router-dom";
+
+interface FormData{
+  personalInfo:{
+    firstname:string;
+    lastname:string;
+    email:string;
+  }
+}
+
+interface FormAction{
+  type:"UPDATE_PERSONAL_INFO",
+  payload:Partial<FormData["personalInfo"]>,
+}
+
+const initialPersonalInfo: FormData["personalInfo"]={
+  firstname:"",
+  lastname:"",
+  email:"",
+};
 
 const PersonalInfo = () => {
+  
+  const [state, dispatch] = useReducer(personalInfoReducer, initialPersonalInfo);
+
+  const navigate = useNavigate();
+
+  function personalInfoReducer(state=initialPersonalInfo, action:FormAction ){
+    return {...state, ...action.payload};
+  }
+
+  function handleSubmit(e:React.FormEvent<HTMLFormElement>){
+    e.preventDefault();
+    console.log(state); // -> {Fname: --- , Lname: ---, email: --@gmail.com}
+    navigate("/address");
+  }
+
   return (
-    <form>
+    <form onSubmit={(e)=>handleSubmit(e)}>
       <h1>Personal Information</h1>
 
       <div>
         <label htmlFor="firstname">First Name</label>
-        <input type="text" id="firstname" required/>
+        <input type="text" id="firstname" required
+        value={state.firstname}
+        onChange={(e)=>
+          dispatch({
+            type:"UPDATE_PERSONAL_INFO",
+            payload:{firstname: e.target.value},
+          })
+        }
+        />
       </div>
 
       <div>
         <label htmlFor="lastname">Last Name</label>
-        <input type="text" id="lastname" required/>
+        <input type="text" id="lastname" required
+        value={state.lastname}
+         onChange={(e)=>
+          dispatch({
+            type:"UPDATE_PERSONAL_INFO",
+            payload:{lastname: e.target.value},
+          })
+        }
+        />
       </div>
 
       <div>
         <label htmlFor="email">Email</label>  
-        <input type="email" id="email" required/>
+        <input type="email" id="email" required
+        value={state.email}
+         onChange={(e)=>
+          dispatch({
+            type:"UPDATE_PERSONAL_INFO",
+            payload:{email: e.target.value},
+          })
+        }
+        />
       </div>
 
       <button type="submit">Next</button>
